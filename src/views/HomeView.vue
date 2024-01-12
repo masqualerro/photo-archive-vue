ac77a3ff86654a2f4e8b7a07cde90e6448c5fe10
masqualerro <86025634+masqualerro@users.noreply.github.com>
2024-01-11 16:48:22 -0800

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 border border-green-500">
+    <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
+  </div>
+</template>
